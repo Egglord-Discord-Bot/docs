f2d30dd54b4e27f33e5f5b9423e521f63b78f579
Spiderjockey02 <benjamin.forey11@gmail.com>
2024-09-15 14:48:58 +0100

    Fixed copyright disclaimer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -125,7 +125,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Egglord.`,
     },
     prism: {
       theme: prismThemes.github,
